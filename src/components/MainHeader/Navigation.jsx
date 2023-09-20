ea5f65042f2557fad883b88d7b3619c2db15cb05
@NelProgrammer <nelsonlzweni@gmail.com>
2023-09-20 23:44:43 +0200

    Changed to Context API instead of using props to pass data down the BeanStalk.
    you don't need to pass the data inside some dirty props, as long as you indicate the component is a consummer. The only downside is you now have to rest your jsx inside the return statement inside another return statement

--- src/components/MainHeader/Navigation.jsx
+++ src/components/MainHeader/Navigation.jsx
@@ -1,28 +1,34 @@
 import React from 'react';
-
+import AuthContext from '../../store/auth-context';
 import css_Navigation from './Navigation.module.css';
 
 const Navigation = (props) => {
   return (
-    <nav className={css_Navigation.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
+    <AuthContext.Consumer>
+      {(ctx) => {
+        return (
+          <nav className={css_Navigation.nav}>
+            <ul>
+              {ctx.isLoggedIn && (
+                <li>
+                  <a href="/">Users</a>
+                </li>
+              )}
+              {ctx.isLoggedIn && (
+                <li>
+                  <a href="/">Admin</a>
+                </li>
+              )}
+              {ctx.isLoggedIn && (
+                <li>
+                  <button onClick={props.onLogout}>Logout</button>
+                </li>
+              )}
+            </ul>
+          </nav>
+        );
+      }}
+    </AuthContext.Consumer>
   );
 };
 
